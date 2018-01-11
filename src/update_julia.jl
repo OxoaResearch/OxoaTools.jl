2d6d728d393d2c2a2286a0fe0029a5d8249c627b
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-01-11 16:54:42 +0100

    added julia update script

---
+++ src/update_julia.jl
@@ -0,0 +1,101 @@
+#!/bin/julia 
+
+#=
+NAME: update_julia - a small script that installs the latest precompiled Julia nightly lor Linux x86-64
+	
+SYNOPSYS
+ 	update_julia [--dir=<install path>] [--precompile] [--update]
+
+DESCRIPTION
+	The script downloads the latest Julia nightly from http://julialang.org and installs it in 
+	/opt if --dir is not used. It also generates a sylink named 'julia_nightly' in /usr/local/bin.
+OPTIONS
+	--dir=<install path>
+		Installs julia in the path specified, provided that the path exists. If the path
+		does not exists, the installation aborts.
+	--precompile
+		Creates a Julia system image by calling build_sysimg.jl
+	--update
+		Runs a Pkg.update() after installation
+AUTHOR
+
+Written by Corneliu Cofaru, (c) 2018
+=#
+
+
+@show nprocs()
+@show ARGS
+
+# Keep track of input arguments
+JULIA_INST = "/opt"		# installation directory (has to exist)
+PRECOMPILE = false;		# pre-compilation flag 
+UPDATE = false; 		# update flag
+
+# Parse arguments
+if length(ARGS) >0
+	for (i, arg) in enumerate(ARGS)
+		arg == "--precompile" 	&& begin PRECOMPILE = true end
+		arg == "--update" 	&& begin UPDATE = true end
+		if contains(arg, "--dir=")
+			JULIA_INST = split(arg,"=")[2]
+			!ispath(JULIA_INST) && begin
+				println("Install path \"$JULIA_INST\" does not exist. Aborting...")
+				exit(-1)	
+			end
+		end
+	end
+end
+@show ARGS, JULIA_INST, PRECOMPILE, UPDATE
+
+# Initialize constants 
+JULIA_LINK = "https://julialangnightlies-s3.julialang.org/bin/linux/x64/julia-latest-linux64.tar.gz"	# download link
+JULIA_DWL_FILE = "/tmp/julia_latest_tmp.tar.gz"				# save file
+
+# Clean /opt
+println("Pre-cleaning...")
+files_opt = open(readlines, `ls $JULIA_INST`)
+files_to_remove = files_opt[ find([contains(s,"julia-")||isequal(s, "julia") for s in files_opt]) ]
+for fname in files_to_remove
+	println("\tdeleteing $(fname)...")
+	run(`rm -rf $(joinpath(JULIA_INST,fname))`)
+end
+
+# Download and extract
+println("Downloading...")
+run(`wget $(JULIA_LINK) --output-document=$(JULIA_DWL_FILE)`)
+
+println("Extracting...")
+run(`tar zxvf $(JULIA_DWL_FILE) -C $(JULIA_INST)`)
+run(`rm $(JULIA_DWL_FILE)`)
+
+# Create /opt/julia symlink
+println("Sym-linking...")
+JULIA_VER = open(readlines, pipeline(`ls /opt/. --sort=time`,`grep julia-`))
+run(`ln -s $(joinpath(JULIA_INST,JULIA_VER[1])) $(joinpath(JULIA_INST,"julia"))`)
+
+# Precompile if specified
+if PRECOMPILE 
+	ans="Y" 
+else   
+	print("Precompile (Y/n)? : "); 
+	ans = readline(STDIN, chomp=true)
+end
+if (ans != "n") && (ans != "N")
+	precompile_cmd = "include(\"/opt/julia/share/julia/build_sysimg.jl\");build_sysimg(default_sysimg_path(), \"native\", nothing;force=true);"
+	run(`$(joinpath(JULIA_INST,"julia/bin/julia")) -e $(precompile_cmd)`)
+end
+
+# Update packages if specified
+if UPDATE 
+	ans="Y"
+else
+	print("Run a Pkg.update() (Y/n)? : "); 
+	ans = readline(STDIN, chomp=true)
+end
+
+if (ans != "n") && (ans != "N")
+	pkgup_cmd = "Pkg.update()"
+	run(`$(joinpath(JULIA_INST,"julia/bin/julia")) -e $(pkgup_cmd)`)
+end
+
+println("Installation of julia $(split(JULIA_VER[1],"-")[2]) done.")
