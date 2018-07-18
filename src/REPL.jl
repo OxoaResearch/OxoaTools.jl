13476df079b5039b9c9f7e2cab4579753c2c7315
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-07-18 17:12:55 +0200

    Added wuye REPL theme

--- src/REPL.jl
+++ src/REPL.jl
@@ -43,11 +43,12 @@ function colorscheme_elflord()
 	return color_scheme
 end
 
+
 function colorscheme_Mustang()
 	color_scheme = OhMyREPL.Passes.SyntaxHighlighter.ColorScheme()
 
 	# Keyword 
-	OhMyREPL.Passes.SyntaxHighlighter.keyword!(color_scheme, Crayons.Crayon(foreground = 208, bold=false))
+	OhMyREPL.Passes.SyntaxHighlighter.keyword!(color_scheme, Crayons.Crayon(foreground = :050, bold=false))
 
 	# Number 
 	OhMyREPL.Passes.SyntaxHighlighter.number!(color_scheme, Crayons.Crayon(foreground = :208, bold=true))
@@ -62,7 +63,7 @@ function colorscheme_Mustang()
 	OhMyREPL.Passes.SyntaxHighlighter.string!(color_scheme, Crayons.Crayon(foreground = :149, bold=true))
 
 	# Operators 
-	OhMyREPL.Passes.SyntaxHighlighter.op!(color_scheme, Crayons.Crayon(foreground = :105, bold=false))
+	OhMyREPL.Passes.SyntaxHighlighter.op!(color_scheme, Crayons.Crayon(foreground = :254, bold=false))
 
 	# Macros 
 	OhMyREPL.Passes.SyntaxHighlighter.macro!(color_scheme, Crayons.Crayon(foreground = :191, bold=true))
@@ -85,6 +86,49 @@ function colorscheme_Mustang()
 	return color_scheme
 end
 
+
+function colorscheme_wuye()
+	color_scheme = OhMyREPL.Passes.SyntaxHighlighter.ColorScheme()
+
+	# Keyword 
+	OhMyREPL.Passes.SyntaxHighlighter.keyword!(color_scheme, Crayons.Crayon(foreground = :086, bold=true))
+
+	# Number 
+	OhMyREPL.Passes.SyntaxHighlighter.number!(color_scheme, Crayons.Crayon(foreground = :051, bold=true))
+
+	# Text 
+	OhMyREPL.Passes.SyntaxHighlighter.text!(color_scheme, Crayons.Crayon(foreground = :252, bold=false))
+
+	# Symbol 
+	OhMyREPL.Passes.SyntaxHighlighter.symbol!(color_scheme, Crayons.Crayon(foreground = :199, bold=true))
+
+	# String
+	OhMyREPL.Passes.SyntaxHighlighter.string!(color_scheme, Crayons.Crayon(foreground = :199, bold=true))
+
+	# Operators 
+	OhMyREPL.Passes.SyntaxHighlighter.op!(color_scheme, Crayons.Crayon(foreground = :51, bold=false))
+
+	# Macros 
+	OhMyREPL.Passes.SyntaxHighlighter.macro!(color_scheme, Crayons.Crayon(foreground = :128, bold=true))
+
+	# Comment
+	OhMyREPL.Passes.SyntaxHighlighter.comment!(color_scheme, Crayons.Crayon(foreground = :244, bold=false))
+
+	# Argument definitions 
+	OhMyREPL.Passes.SyntaxHighlighter.argdef!(color_scheme, Crayons.Crayon(foreground = :039, bold=false))
+
+	# Function definitions 
+	OhMyREPL.Passes.SyntaxHighlighter.function_def!(color_scheme, Crayons.Crayon(foreground = :254, bold=true))
+
+	# Function call 
+	OhMyREPL.Passes.SyntaxHighlighter.call!(color_scheme, Crayons.Crayon(foreground = :051, bold=true))
+
+	# Error 
+	OhMyREPL.Passes.SyntaxHighlighter.error!(color_scheme, Crayons.Crayon(foreground = :196, bold=true))
+
+	return color_scheme
+end
+
 function apply_colorscheme(cs)
 	
 	# Apply colorscheme
@@ -96,14 +140,15 @@ function apply_colorscheme(cs)
 	OhMyREPL.Passes.BracketHighlighter.setcrayon!(c)
 
 	# Bracket autocompletion off
-	OhMyREPL.enable_autocomplete_brackets(false)
+	OhMyREPL.enable_autocomplete_brackets(true)
 
 	# Prompt
-	OhMyREPL.input_prompt!("julia>", :light_blue)
+	OhMyREPL.input_prompt!(">", :light_green)
 	#OhMyREPL.output_prompt!("", :white)
 end
 
 # Apply colorscheme
 # One can view the available colors with > Crayons.test_256_colors()
-cs = colorscheme_Mustang()
+#cs = colorscheme_Mustang()
+cs = colorscheme_wuye()
 apply_colorscheme(cs)
