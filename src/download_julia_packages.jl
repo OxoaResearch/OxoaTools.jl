9434236b3b70d7c4bce8f9cb36407a41fa94c877
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-10-08 19:53:41 +0200

    First version

--- src/download_julia_packages.jl
+++ src/download_julia_packages.jl
@@ -4,8 +4,8 @@
 NAME: download_julia_packages - download all registered Julia packages in one go.
 	
 SYNOPSYS
- 	download_julia_packages [--dir <path>] [--archive <option>] [--update]
-                            [--julia-dir <path>] [--registry <path to 'Registry.toml'>]
+    download_julia_packages [--dir <path>] [--archive <option>] [--julia-dir <path>]
+                            [--registry <path to 'Registry.toml'>]
 
 DESCRIPTION
 	The script downloads the latest Julia nightly from http://julialang.org and installs it in 
@@ -16,21 +16,27 @@ OPTIONS
         it is created.
 	--archive
 		Additionally creates a '.tar.gz' archive with all packages.
-	--update
-		Updates all Julia packages that exist at the specified path running 'git pull' and
-        downloads new packages.
     --julia-dir <path>
-        The path to the '.julia' folder; this is used for accessing the 'Registry.toml' file.
+        The path to the '.julia' folder; this is used for accessing package information.
     --registry <path>
-        Custom path for the 'Registry.toml' file; has priority over the '--julia-dir' value.
+        Custom path for the 'Registry.toml' file.
   
 AUTHOR
 
 Written by Corneliu Cofaru, (c) 2018
 =#
 
+# TODO(Corneliu): Add update option (i.e. incremental download + update existing)
+# TODO(Corneliu): Integrate with LibGit2 to perform operations.
+# TODO(Corneliu): Get/update Registry.toml by default without needing a Julia
+# installation path or path to registry toml; the options can be used as
+# overrides only.
 
 
+
+using Pkg
+installed = Pkg.installed()
+!("ArgParse" in keys(installed)) &&  Pkg.add("ArgParse")
 using ArgParse
 
 
@@ -45,22 +51,18 @@ function get_commandline_arguments(args::Vector{String})
         "--archive"
             help = "archive the downloaded repositories into a '.tar.gz' archive"
             action = :store_true
-        "--update"
-            help = ""
-            action = :store_true
         "--julia-dir"
             help = "path to the '.julia' folder"
-            default = ""
+            default = abspath(expanduser("~/.julia"))
         "--registry"
             help = "path to the 'Registry.toml' file"
-            default = ""
+            default = abspath(expanduser("~/.julia/registries/General/Registry.toml"))
     end	
     return parse_args(args,s)
 end
 
 
 
-#TODO(Corneliu): Integrate with LibGit2 to perform operations.
 function main(ARGS::Vector{String})
 	
 	# Parse command line arguments
@@ -68,39 +70,84 @@ function main(ARGS::Vector{String})
 
 	DIR = args["dir"]
 	ARCHIVE = args["archive"]
-	UPDATE = args["update"]
     JULIA_DIR = args["julia-dir"]
     REGISTRY = args["registry"]
 
-    # TODO(Corneliu): Get/update Registry.toml by default without needing a Julia
-    # installation path or path to registry toml; the options can be used as
-    # overrides only.
-    
-    @warn "No functionality implemented yet."
-    
     # Create installation path if it does not exist
+    directory = abspath(DIR)
+    !isdir(directory) && mkpath(directory)
 
-    # Decide the path for 'Registry.toml'
+    # Check the Julia directory for registries
+    pkg_reg_path = abspath(joinpath(JULIA_DIR, "registries", "General"))
+    !isdir(pkg_reg_path) && @error "$pkg_reg_path does not exist."
+
+    # Get the Registry.toml path
+    registry_path = joinpath(pkg_reg_path, "Registry.toml")
+    if !isfile(registry_path)
+        @warn "Registry.toml not found in $JULIA_DIR."
+        if !isfile(REGISTRY)
+            @error "Registry.toml not found in $REGISTRY."
+        else
+            registry_path = abspath(REGISTRY)
+        end
+    end
 
     # Parse file and extract information in a Vector of tuples (package_name, path/to/Package.toml)  
+    @info "Parsing $registry_path..."
+
+    # Function that parses the line in the Registry.toml file
+    function _parse_registry_line(line)
+        _, _tmp1 = split(line, "{");
+        _tmp1=replace(_tmp1, r"(\"|})"=>"");
+        _name, _path = split(_tmp1, ",")
+        _, name = strip.(split(_name, "="))
+        _, path = strip.(split(_path, "="))
+        return name, path
+    end
     
-    # If no 'update'
-        
-        # For each element of the vector, retrieve from the 'Package.toml' the GitHub package path
-        # and create a new Vector of tuples (package_name, GitHub path)
+    function _git_clone(repo, path)
+        run(pipeline(`git clone $repo $path`, stdout=devnull))
+    end
 
-        # Download packages asynchronously (try-catch) and store the result of the download somewhere
+    PkgInfos = Vector{Tuple{String, String}}()
+    open(registry_path, "r") do fid_registry
+        # Inside Registry.toml
+        for line in eachline(fid_registry)
+            if all(occursin(symbol,line) for symbol in ["{", "}", "="])
+                #00701ae9-d1dc-5365-b64a-a3a3ebf5695e = { name = "BioAlignments", path = "B/BioAlignments" }
+                name, regdir = _parse_registry_line(line)
+                open(joinpath(pkg_reg_path, regdir, "Package.toml"), "r") do fid_package
+                    # Inside Package.toml
+                    for _line in eachline(fid_package)
+                        key, val = strip.(split(_line, "="))
+                        if key == "repo"
+                            push!(PkgInfos, (name, replace(val, "\""=>"")))
+                        end
+                    end
+                end
+            end
+        end
+    end
 
-        # Do another retry and write down a list of downloaded package names that exist 
-    # else
-        
-        # Compare existing package list with the one extracted from 'Registry.toml'
-        
-        # Download non-existing packages
-        
-        # Update the rest
+    @info "Cleaning up $directory ..."
+    directories = readdir(directory)
+    if !isempty(directories)
+        for _dir in directories
+            @info "removing $_dir"
+            Base.Filesystem.rm(joinpath(directory,_dir), recursive=true, force=true)
+        end
+    end
 
-    # Create archive if specified
+    @info "Cloning repositories..."
+    CONCURRENT = 8
+    asyncmap(PkgInfos[1:15], ntasks=CONCURRENT) do x
+        _git_clone(x[2], joinpath(DIR, x[1]))
+    end
+
+    if ARCHIVE
+        @info "Compressing repositories"
+        run(pipeline(`tar czf ./all_julia_repositories.tar.gz $directory`, stdout=devnull))
+    end
 end
 
 
